ae7031033c716223e1506ae1cbe02c823e92d8db
Space <sreyasvinta@gmail.com>
2023-11-17 18:29:58 -0600

    listing generator

---
+++ make listing.py
@@ -0,0 +1,39 @@
+# Number.
+# IGN: 
+# Discord: []
+# Cheats used: 
+# Date: 
+# Caught by: 
+# Positive?: 
+# Comment: 
+# Evidence:
+import os
+import subprocess
+os.system('cls')
+
+number = input("listing #: ")
+ign = input("IGN: ")
+discordUsername, id = input("discord (username, id): ").split(", ")
+cheatsUsed = input("Cheats used: ")
+date = input("Date (month/day/year): ")
+caughtBy = input("Caught by: ")
+positivity = input("Positive?: ")
+comment = input("Comment: ")
+
+listing = f"""
+{number}.
+IGN: {ign}
+Discord: {discordUsername} [{id}]
+Cheats used: {cheatsUsed}
+Date: {date}
+Caught by: {caughtBy}
+Positive?: {positivity}%
+Comment: {comment}
+Evidence:
+
+
+[original evidence]
+
+"""
+print(listing)
+subprocess.run(f"echo {listing}|clip", shell=True) # copy to clipboard (Windows). This is currently not working. I have an idea as to why but I don't care enough rn to deal with it
